f1b9c647fa60b9e28ae15a8f1b996ec362e9e06f
samsaq <sameereza@gmail.com>
2023-05-07 03:23:02 +0800

    Adding svelte-particles & prettifying search page

--- The_Site/SweatySvelting/src/lib/SearchDisplay.svelte
+++ The_Site/SweatySvelting/src/lib/SearchDisplay.svelte
@@ -1,63 +1,78 @@
 <!--
     The component that encloses the search bar & results display
     the results will appear once a search is sent, and will disappear once the search bar is empty
-    the results is a paginated list of stepper cards for each result (displaying 10 per page by default for 5 pages)
-    The first step will be the description, second will be keywords & frequencies, and third will be links
-    Alternatively, we could do a card with an accordion for the keywords & links
-    Displayed in a grid with 3 columns, with a centered search bar with chips on top
-    The center grid is clear and the the two grids on the side are blurred out
-    The other grids are made visible and rotated in when the page buttons are pressed
+    the results is a paginated list
 -->
 <script lang="ts">
-    import { Stepper, Step } from "@skeletonlabs/skeleton";
 
     let resultsReady: boolean = false;
 
-    function onNextHandler(e: any): void {
-        console.log("event:next", e.detail);
-    }
-    function onBackHandler(e: any): void {
-        console.log("event:prev", e.detail);
-    }
-    function onStepHandler(e: any): void {
-        console.log("event:step", e.detail);
-    }
-    function onCompleteHandler(e: any): void {
-        console.log("event:complete", e.detail);
-        alert("Complete!");
-    }
 </script>
 
 {#if resultsReady}
     <div id="resultsContainer" class="">
-        <div class="w-full card p-4 text-token">
-            <Stepper
-                on:next={onNextHandler}
-                on:back={onBackHandler}
-                on:step={onStepHandler}
-                on:complete={onCompleteHandler}
-            >
-                <Step stepTerm="Description">
-                    <svelte:fragment slot="header">
-                        <!--The title of the page & an avatar of its icon (serving as the link)-->
-                        <!--How to display its rank?-->
-                    </svelte:fragment>
-                    <!--We'll display the last modified date & other metadata here-->
-                </Step>
-                <Step stepTerm="Keywords">
-                    <svelte:fragment slot="header"
-                        >Keywords & Frequencies</svelte:fragment
-                    >
-                    <!--A ul going over a parsed list of Keywords will go here-->
-                </Step>
-                <Step stepTerm="Links">
-                    <svelte:fragment slot="header">Child Links</svelte:fragment>
-                    <!--A ul going over a parsed list of links will go here-->
-                </Step>
-            </Stepper>
-        </div>
+        
     </div>
 {/if}
 
-<style lang="postcss">
+<style lang="scss">
+    @function gray($color) {
+        @return rgb($color, $color, $color);
+    }
+
+    $shadow: rgba(0, 0, 0, 0.12) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px;
+    $cardShadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+        rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+
+    $red: rgb(237, 66, 69);
+
+    @keyframes fadeInAndExpand {
+        from {
+            opacity: 0;
+            transform: scale(0.25);
+        }
+
+        50% {
+            opacity: 0;
+        }
+
+        to {
+            opacity: 1;
+            transform: scale(1);
+        }
+    }
+
+    @keyframes fadeInFromLeft {
+        from {
+            opacity: 0;
+            transform: translateX(-20px);
+        }
+
+        50% {
+            opacity: 0;
+        }
+
+        to {
+            opacity: 1;
+            transform: translateX(0px);
+        }
+    }
+
+    @keyframes spin {
+        from {
+            transform: rotate(0deg);
+        }
+
+        50% {
+            transform: rotate(720deg);
+        }
+
+        to {
+            transform: rotate(1440deg);
+        }
+    }
+
+    .spin-animation {
+        animation: spin 2s ease-in-out infinite;
+    }
 </style>
--- The_Site/SweatySvelting/src/routes/Search/+page.svelte
+++ The_Site/SweatySvelting/src/routes/Search/+page.svelte
@@ -1,51 +1,156 @@
 <script lang="ts">
     import { onMount, onDestroy } from "svelte";
     import SearchBarAndPaginator from "$lib/SearchBarAndPaginator.svelte";
-    import { AppBar, ProgressRadial, drawerStore } from "@skeletonlabs/skeleton";
+    import {
+        AppBar,
+        ProgressRadial,
+        drawerStore,
+    } from "@skeletonlabs/skeleton";
     import type { DrawerSettings } from "@skeletonlabs/skeleton";
     import { searchHistory, searchQuery } from "../../historyStore";
-    import 'iconify-icon';
+    import "iconify-icon";
+    import type { CustomEventWrapper, ParticlesEvents } from "svelte-particles";
+    import type { Engine } from "tsparticles-engine";
+    import type svelteParticles from "svelte-particles";
+    import { loadFull } from "tsparticles";
+
+    let ParticlesComponent: any;
+
+    const particlesConfig = {
+        particles: {
+            number: {
+                value: 30,
+            },
+            color: {
+                value: "#607d8b",
+            },
+            size: {
+                value: 2,
+            },
+            move: {
+                enable: true,
+                speed: 2,
+                outModes: {
+                    default: "bounce",
+                },
+            },
+            line_linked: {
+                enable: true,
+                distance: 250,
+                color: "#607d8b",
+            },
+        },
+    };
+
+    let onParticlesLoaded = (event: any) => {
+        const particlesContainer = event.detail.particles;
+    };
+
+    let particlesInit = async (main: Engine) => {
+        // you can use main to customize the tsParticles instance adding presets or custom shapes
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(main);
+    };
+
+    onMount(async () => {
+        //dynamic import for svelte-particles so it can be used client side (prevents window not defined crash)
+        const particlesModule = await import("svelte-particles"); // thats why we add the particles to each side in onMount after page load
+
+        ParticlesComponent = particlesModule.default;
+    });
 
     let curQuery: string = "";
     let waitingForSearch: boolean = true; // to trigger the loading animation
 
     //function to append used search queries to the history store's searchHistory array
-    function appendSearchQuery (newQuery: string) {
+    function appendSearchQuery(newQuery: string) {
         searchHistory.update((arr) => [...arr, newQuery]);
     }
 
-    function updateSearchQuery (newQuery: string) {
+    function updateSearchQuery(newQuery: string) {
         searchQuery.set(newQuery);
     }
-
 </script>
 
 <main>
-
-    <AppBar background = "variant-ghost-error">
+    <AppBar background="variant-ghost-error">
         <svelte:fragment slot="lead">
             <a href="/">
-                <iconify-icon icon="ic:outline-home" height=2rem width=2rem style="color: #000000"></iconify-icon>
+                <iconify-icon
+                    icon="ic:outline-home"
+                    height="2rem"
+                    width="2rem"
+                    style="color: #000000"
+                />
             </a>
         </svelte:fragment>
-        <svelte:fragment slot= "trail">
+        <svelte:fragment slot="trail">
             <!--We'll put the hamburger for the history menu here-->
-            <button on:click="{() => drawerStore.open()}">
+            <button on:click={() => drawerStore.open()}>
                 <img src="/history-linear.svg" alt="History" />
             </button>
         </svelte:fragment>
     </AppBar>
 
+    <div id="particles-container">
+        <svelte:component
+            this={ParticlesComponent}
+            id="particles"
+            options={particlesConfig}
+            on:particlesLoaded={onParticlesLoaded}
+            {particlesInit}
+        />
+    </div>
+
+    <div id="search-content-container">
+        <div class= "search-hero flex flex-col border-l-4 border-black p-4">
+            <h2 class="search-step">Let's</h2>
+            <h2 class="search-step">Search for</h2>
+            <h2 class="search-step"><span class="fancy">Connections</span></h2>
+        </div>
+    </div>
+
 </main>
 
-<style>
-
+<style lang="scss">
     main {
         background-color: rgb(var(--color-error-500));
         margin: 0px;
         height: 100vh;
         width: 100vw;
         overflow: hidden;
+    }
+
+    #particles-container {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+    }
+
+    .search-step {
+        font-family: "Rubik";
+        font-size: 8vw;
+        margin-top: .75rem;
+        margin-bottom: .75rem;
+
+        .fancy {
+            font-family: "Lobster", cursive;
+            color: rgb(var(--color-primary-500));
+        }
+    }
+
+    #search-content-container {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    position: absolute;
+    color: black;
+    pointer-events: none;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
     }
 
 </style>
