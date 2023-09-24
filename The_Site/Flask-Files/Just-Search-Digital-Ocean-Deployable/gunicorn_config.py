33c39b555003ee38ffcee6fa09a6970e42cb931f
samsaq <sameereza@gmail.com>
2023-09-24 14:35:39 -0700

    chore: folder rename

--- The_Site/Flask-Files/Just-Search-Digital-Ocean-Deployable/gunicorn_config.py
+++ The_Site/Flask-Files/Just-Search-Digital-Ocean-Deployable/gunicorn_config.py
@@ -3,7 +3,7 @@ import os
 workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
 threads = int(os.environ.get('GUNICORN_THREADS', '2'))
 timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
-bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
+bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:443')
 
 certfile = os.environ.get('GUNICORN_CERTFILE', '/etc/letsencrypt/live/search-engine-api.fly.dev/fullchain.pem')
 keyfile = os.environ.get('GUNICORN_KEYFILE', '/etc/letsencrypt/live/search-engine-api.fly.dev/privkey.pem')
